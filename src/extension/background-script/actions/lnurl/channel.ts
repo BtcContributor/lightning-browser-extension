d64b08ebc4547f4218bfdf048450db111f51c2f9
escapedcat <github@htmlcss.de>
2022-07-20 13:50:04 +0800

    feat(lnurl): support channelRequest #738

--- src/extension/background-script/actions/lnurl/channel.ts
+++ src/extension/background-script/actions/lnurl/channel.ts
@@ -1,5 +1,4 @@
 import utils from "~/common/lib/utils";
-// import state from "~/extension/background-script/state";
 import type { Message, LNURLDetails } from "~/types";
 
 async function channelRequestWithPrompt(
@@ -7,19 +6,6 @@ async function channelRequestWithPrompt(
   lnurlDetails: LNURLDetails
 ) {
   try {
-    // const connector = await state.getState().getConnector();
-    //   {
-    //     "uri": string, // Remote node address of form node_key@ip_address:port_number
-    //     "callback": string, // a second-level URL which would initiate an OpenChannel message from target LN node
-    //     "k1": string, // random or non-random string to identify the user's LN WALLET when using the callback URL
-    //     "tag": "channelRequest" // type of LNURL
-    // }
-    //LNURLDetails
-    // split uri
-    // hand host/pubkey into connector
-    // if no error then callback-url with "k1"-string and our node id (get via getinfo)
-    // connector.connectPeer();
-
     const response = await utils.openPrompt({
       origin: message.origin,
       action: "lnurlOpenChannel",
